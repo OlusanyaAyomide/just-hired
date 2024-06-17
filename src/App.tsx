db7bad2628d5d7e7a433556b654dd38a046545c2
Olusanya Ayomide <ayomideflex72@gmail.com>
2024-06-17 08:31:35 +0100

    Added code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,52 @@
 import {BrowserRouter,Route,Routes,
 } from 'react-router-dom';
-import LogIn from './pages/auth/LogIn';
-import Dashboard from './pages/protected/dashboard/DashBoard';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import NewPassword from './pages/auth/NewPassword';
-import Recruitment from './pages/protected/recruitment/Recruitment';
-import RecruitmentAdd from './pages/protected/recruitment/RecruitmentAdd';
-import AddCountry from './pages/protected/country/AddCountry';
-import PaymentDashBoard from './pages/protected/dashboard/PaymentDashBoard';
-import CountryList from './pages/protected/country/CountryList';
-import CountryEdit from './pages/protected/country/CountryEdit';
-// import AddEducation from './pages/protected/education/AddEducation';
-// import EditEducatiion from './pages/protected/education/EditEducation';
-// import EducationList from './pages/protected/education/EduactionList';
-// import AddExperience from './pages/protected/experience/AddExperience';
-// import ExperienceList from './pages/protected/experience/ExperienceList';
-// import EditExperience from './pages/protected/experience/EditExperience';
+import { lazy, Suspense } from 'react';
+const LogIn = lazy(()=> import('./pages/auth/LogIn'));
+const Dashboard = lazy(()=> import('./pages/protected/dashboard/DashBoard'));
+const ForgotPassword = lazy(()=> import('./pages/auth/ForgotPassword'));
+const NewPassword = lazy(()=> import('./pages/auth/NewPassword'));
+const Recruitment = lazy(()=> import('./pages/protected/recruitment/Recruitment'));
+const RecruitmentAdd = lazy(()=> import('./pages/protected/recruitment/RecruitmentAdd'));;
+const AddCountry = lazy(()=> import('./pages/protected/country/AddCountry'));;
+const PaymentDashBoard = lazy(()=> import('./pages/protected/dashboard/PaymentDashBoard'));
+const CountryList = lazy(()=> import('./pages/protected/country/CountryList'));
+const CountryEdit = lazy(()=> import('./pages/protected/country/CountryEdit'));
+// const AddEducation = lazy(()=> import('./pages/protected/education/AddEducation'));
+// const EditEducatiion = lazy(()=> import('./pages/protected/education/EditEducation'));
+// const EducationList = lazy(()=> import('./pages/protected/education/EduactionList'));
+// const AddExperience = lazy(()=> import( './pages/protected/experience/AddExperience'));
+// const ExperienceList = lazy(()=> import( './pages/protected/experience/ExperienceList'));
+// const EditExperience = lazy(()=> import( './pages/protected/experience/EditExperience'));
 
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard/>} />
-      <Route path='/dashboard/payment' element ={<PaymentDashBoard/>}/>
-      <Route path="/login" element={<LogIn/>} />
-      <Route path="/forgot-password" element={<ForgotPassword/>} />
-      <Route path='/new-password' element={<NewPassword/>}/>
+    <Suspense fallback={<div className='text-5xl font-bold grid place-items-center h-screen'>Loading...</div>}>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path='/dashboard/payment' element ={<PaymentDashBoard/>}/>
+        <Route path="/login" element={<LogIn/>} />
+        <Route path="/forgot-password" element={<ForgotPassword/>} />
+        <Route path='/new-password' element={<NewPassword/>}/>
 
-      <Route path='/admin/country/add' element={<AddCountry/>}/>
-      {/* NextUpdate */}
-      <Route path='/admin/country/view' element={<CountryList/>}/>
-      <Route path='/admin/country/edit/:id' element={<CountryEdit/>}/>
-      {/* Next Update */}
+        <Route path='/admin/country/add' element={<AddCountry/>}/>
+        <Route path='/admin/country/view' element={<CountryList/>}/>
+        <Route path='/admin/country/edit/:id' element={<CountryEdit/>}/>
+        {/* NextUpdate */}
+        {/* <Route path='/admin/education/add' element={<AddEducation/>}/>
+        <Route path='/admin/education/view' element={<EducationList/>}/>
+        <Route path='/admin/education/edit/123' element={<EditEducatiion/>}/> */}
+        {/* Next Update */}
+        {/* <Route path='/admin/experience/add' element={<AddExperience/>}/>
+        <Route path='/admin/experience/view' element={<ExperienceList/>}/>
+        <Route path='/admin/experience/edit/123' element={<EditExperience/>}/> */}
 
-      {/* <Route path='/admin/education/add' element={<AddEducation/>}/>
-      <Route path='/admin/education/view' element={<EducationList/>}/>
-      <Route path='/admin/education/edit/123' element={<EditEducatiion/>}/>
+        <Route path='/recruit/company/view' element={<Recruitment/>}/>
+        <Route path='/recruit/company/add' element={<RecruitmentAdd/>}/>
+        <Route path='*' element={<div className='text-shade text-2xl pt-20'>Not Found</div>}/>
+      </Routes>
+    </Suspense>
 
-      <Route path='/admin/experience/add' element={<AddExperience/>}/>
-      <Route path='/admin/experience/view' element={<ExperienceList/>}/>
-      <Route path='/admin/experience/edit/123' element={<EditExperience/>}/> */}
-      
-      <Route path='/recruit/company/view' element={<Recruitment/>}/>
-      <Route path='/recruit/company/add' element={<RecruitmentAdd/>}/>
-      <Route path='*' element={<div className='text-shade text-2xl pt-20'>Not Found</div>}/>
-    </Routes>
   );
 }
 
